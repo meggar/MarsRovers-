348b5048b4a2f6b26ecbce07086fe9b1d8641cfe
Mike Eggar <mikeeggar@gmail.com>
2019-01-05 22:16:41 -0500

    Added very simple non-fake nasa api datasource.

---
+++ MarsRovers!/DataSources/NasaRoverPhotoAPI.swift
@@ -0,0 +1,44 @@
+//
+//  NasaMarsRoverAPI.swift
+//  MarsRovers!
+//
+//  Created by Mike Eggar on 1/5/19.
+//  Copyright © 2019 Mike Eggar. All rights reserved.
+//
+
+import Foundation
+
+class NasaRoverPhotoAPI: RoverPhoto_DataSource {
+    
+    
+    static let routes:[RoverType:String] = [
+        .curiosity: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos",
+        .opportunity: "https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos",
+        .spirit: "https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos"
+    ]
+    
+    func getPhotosFor(rover: RoverType, completion: @escaping (Photos?) -> ()) {
+        
+        if let earthDate = DataSourceHelpers.formattedDateString(daysAgo: 2000),
+            let apiKey = DataSourceHelpers.apiKeyFromPlist(),
+            let route = NasaRoverPhotoAPI.routes[rover],
+            let url = URL(string: route) {
+        
+            var request = URLRequest(url: url)
+            request.addValue(apiKey, forHTTPHeaderField: "api_key")
+            request.addValue(earthDate, forHTTPHeaderField: "earth_date")
+            
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                
+                if let data = data {
+                    completion(try? JSONDecoder().decode(Photos.self, from: data))
+                }
+                
+            }
+            
+            task.resume()
+        }
+        
+    }
+
+}
